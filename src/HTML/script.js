64e9c280f397df563c7759030fffbeb444134eec
SLAY/E\DES <97400362+SLAYEDES@users.noreply.github.com>
2022-10-17 21:26:48 +0200

    Add files via upload

---
+++ src/HTML/script.js
@@ -0,0 +1,5 @@
+const progressBar = document.getElementsByClassName('barre')[0]
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar)
+  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+  progressBar.style.setProperty('--width', width + .1)})
