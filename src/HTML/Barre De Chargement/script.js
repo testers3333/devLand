324e83b5e928ab63521475e325e297693aafc013
SLAY/E\DES <97400362+SLAYEDES@users.noreply.github.com>
2022-10-17 21:25:02 +0200

    Delete src/HTML/Barre De Chargement directory

--- src/HTML/Barre De Chargement/script.js
+++
@@ -1,5 +0,0 @@
-const progressBar = document.getElementsByClassName('barre')[0]
-setInterval(() => {
-  const computedStyle = getComputedStyle(progressBar)
-  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-  progressBar.style.setProperty('--width', width + .1)})
