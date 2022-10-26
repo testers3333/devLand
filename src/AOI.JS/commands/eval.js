eaa0a9527dfe0e4558d02c08a555bcc9cea8dc7c
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-10-26 17:49:19 +0200

    Update eval.js

--- src/AOI.JS/commands/eval.js
+++ src/AOI.JS/commands/eval.js
@@ -3,5 +3,26 @@ module.exports ={
     aliases:['e'],
     code:`
     $eval[$message;yes]
+    $onlyForIDs[$botOwnerID;Seul le développeur peut utiliser cette commande]
+    `
+  }
+// and if you want eval this: $createApplicationCommand[$guildID;eval;permet de tester une commande à partir d'une autre;true;slash;{
+//             "name" : "code",
+//             "description" : "le code à tester",
+//             "type" : 3,
+//             "required" : true
+//    }] and the code will be :
+module.exports ={
+    name:"eval",
+    type:"interaction",
+    prototype:"slash",
+    code:`
+    $interactionReply[$eval[$interactionData[options.data[0].value];yes]]
+    $onlyForIDs[$botOwnerID;{
+    "content" : "Seul le développeur peut utiliser cette commande",
+    "ephemeral" : true,
+    "options" : {
+    "interaction" : true
+    }}
     `
   }
