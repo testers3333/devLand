18da2a1db6b2abf95eb2fd3cf3958988575a051d
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-11-20 13:10:46 +0100

    Delete src/AOI.JS/commands directory

--- src/AOI.JS/commands/eval.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    name:"eval",
-    aliases:['e'],
-    code:`
-    $eval[$message;yes]
-    $onlyForIDs[$botOwnerID;Seul le développeur peut utiliser cette commande]
-    `
-  }
