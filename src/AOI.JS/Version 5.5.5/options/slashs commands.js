16a1859bc8c089b322605c1c6c435e6c88788ada
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-11-28 18:31:28 +0100

    Update slashs commands.js

--- src/AOI.JS/Version 5.5.5/options/slashs commands.js
+++ src/AOI.JS/Version 5.5.5/options/slashs commands.js
@@ -7,7 +7,7 @@ module.exports ={
     $interactionReply[The answer when using slash] 
     `
   }
-// the code to eval for the code on top : $createApplicationCommand[$guildID;name of slash;description of slash;true]
+// the code to be evaluated for the code at the top : $createApplicationCommand[$guildID;name of slash;description of slash;true]
 module.exports ={
     name:"name of slash",
     type:"interaction",
@@ -16,7 +16,7 @@ module.exports ={
     $interactionReply[Your text: $interactionData[options.data[0].value]] 
     `
   }
-/* the code to eval for the code on top : $createApplicationCommand[$guildID;name of slash;description of slash;true;slash;{
+/* the code to be evaluated for the code at the top : $createApplicationCommand[$guildID;name of slash;description of slash;true;slash;{
              "name" : "name of the option",
              "description" : "the description of the option",
              "type" : 3,
@@ -25,5 +25,5 @@ module.exports ={
  type: 3 == message / 4 == number / 5 == True or False in option / 6 == user / 7 == channel / 8 == role / 9 == everything that is mentionable
  for an ephemeral answer: $interactionReply[The answer when using slash;;;;;yes]
  for an embed answer: $interactionReply[;{newEmbed:{description:The answer when using slash}}]
- For more options go to the files "options"
+ For more options go to the files "embeds options"
 */
