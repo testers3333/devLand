e46a3ca82d07b4e090485121aad7e1b8b991d1b0
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-12-10 13:33:48 +0100

    Create function.lua

---
+++ src/Lua/English/function.lua
@@ -0,0 +1,13 @@
+function identity(nickname, first name, age, city)
+    local result = nickname.." ". .first name.." ". .age.." ".. city
+    return result
+end
+print(identity("your nickname", "your first name","your age", "your city"))
+-- function is the function that creates a function in the code
+-- identity is the function name
+-- local is a function that is always put before a variable so that it is not global
+-- result is the variable where the information given will be stored via the identity function
+-- return serves to know what the function will return, here we will return the variable with the information "result"
+-- end is used to close the function
+-- print is used to run anything in your terminal when launching the games
+-- identity(...) is used to give the information in order to store it in the variable "result"
