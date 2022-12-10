3bdf2ed2de9cdf2aaee13e24a045ac4eed6b00c4
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-12-10 13:36:08 +0100

    Update variables.lua

--- src/Lua/French/variables.lua
+++ src/Lua/French/variables.lua
@@ -1,3 +1,3 @@
 Exemple de variable :
-nom variable = 1, -- ou si la value de la variable est un texte vous devez mettre entre " comme ceci: 
+nom variable = 1, -- ou si la valeur de la variable est un texte vous devez mettre entre " comme ceci: 
 nom variable = "texte"
