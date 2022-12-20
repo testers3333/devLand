95e09dc8f388f0234ce30b91dbf10a08fcf18c49
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2022-12-20 16:55:27 +0100

    Create reactionCollector.js

---
+++ src/AOI.JS/Version 5.5.5/commands/basic/reactionCollector.js
@@ -0,0 +1,12 @@
+module.exports =[{
+  name:"reaction",
+  code:`
+$reactionCollector[$channelID;$get[id];everyone;;❌;reac;yes]
+$let[id;$sendMessage[Hello, you know the best aoi.js encoder ?;yes]]`
+},{
+  name:"reac",
+  type:"awaited",
+  code:`
+  $editMessage[$message[1];It's Leref of course]
+  `
+}]
