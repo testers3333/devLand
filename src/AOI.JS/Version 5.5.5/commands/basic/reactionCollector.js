c47680126ef1344522ae71285a5faedcea938bb9
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2023-02-20 19:59:46 +0100

    Update reactionCollector.js

--- src/AOI.JS/Version 5.5.5/commands/basic/reactionCollector.js
+++ src/AOI.JS/Version 5.5.5/commands/basic/reactionCollector.js
@@ -2,11 +2,11 @@ module.exports =[{
   name:"reaction",
   code:`
   $reactionCollector[$channelID;$get[id];everyone;;❌;reac;yes]
-  $let[id;$sendMessage[Hello, do you know the most beautiful mother?;yes]]`
+  $let[id;$sendMessage[Hello, do you know what song makes a goat ?;yes]]`
 },{
   name:"reac",
   type:"awaited",
   code:`
-  $editMessage[$message[1];It's not your mother ahah]
+  $editMessage[$message[1];"Qué miras bobo"]
   `
 }]
