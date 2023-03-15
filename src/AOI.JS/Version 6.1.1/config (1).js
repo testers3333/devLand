51380e1d69a20c31411bc7bbc1f61c4f4a7181d0
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2023-03-15 19:13:49 +0100

    Add files via upload

---
+++ src/AOI.JS/Version 6.1.1/config (1).js
@@ -0,0 +1,20 @@
+module.exports= {
+    Bot: {
+        token: "MTAwODQ3MDY3OTEwMTkxNTMzNg.GYoycB.V_ztF6In3WGMuqpp9DESV9TTh5pXYutK6cKwkU",
+        prefix: "=",
+        intents: ["Guilds", "GuildMembers", "GuildModeration", "GuildBans", "GuildEmojisAndStickers", "GuildIntegrations", "GuildWebhooks", "GuildInvites", "GuildVoiceStates", "GuildPresences", "GuildMessages", "GuildMessageReactions", "GuildMessageTyping", "DirectMessages", "DirectMessageReactions", "DirectMessageTyping", "MessageContent", "GuildScheduledEvents", "AutoModerationConfiguration", "AutoModerationExecution"],
+        events: ["onMessage", "onMessageDelete", "onMessageUpdate", "onMessageDeleteBulk", "onGuildJoin", "onGuildLeave", "onGuildUpdate", "onGuildUnavailable", "onRoleCreate", "onRoleUpdate", "onRoleDelete", "onChannelCreate", "onChannelUpdate", "onChannelDelete", "onChannelPinsUpdate", "onStageInstanceCreate", "onStageInstanceUpdate", "onStageInstanceDelete", "onThreadCreate", "onThreadUpdate", "onThreadDelete", "onThreadListSync", "onThreadMemberUpdate", "onThreadMembersUpdate", "onJoin", "onLeave", "onMemberUpdate", "onMemberAvailable", "onMembersChunk", "onEmojiCreate", "onEmojiDelete", "onEmojiUpdate", "onStickerCreate", "onStickerDelete", "onStickerUpdate", "onBanAdd", "onBanRemove", "onReactionAdd", "onReactionRemove", "onReactionRemoveAll", "onReactionRemoveEmoji", "onVoiceStateUpdate", "onPresenceUpdate", "onTypingStart", "onInteractionCreate", "onApplicationCmdPermsUpdate", "onUserUpdate", "onVariableCreate", "onVariableDelete", "onVariableUpdate", "onFunctionError", "onWebhookUpdate"],
+        database: {
+            type:'default',
+            db:require('dbdjs.db'),
+            path:"./db/",
+            tables:["main"],
+            promisify:false
+        },
+        respondOnEdit:{
+            commands: true
+        },
+        suppressAllErrors: false,
+        desktopPlatform: true
+    }
+}
