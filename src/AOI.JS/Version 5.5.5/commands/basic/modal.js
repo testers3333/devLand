8da274f85733443a382122e522d2633c698b49d0
DIISLAY <97400362+DIISLAY@users.noreply.github.com>
2023-03-22 18:40:52 +0100

    Create modal.js

---
+++ src/AOI.JS/Version 5.5.5/commands/basic/modal.js
@@ -0,0 +1,28 @@
+module.exports =[{
+  name:"modal",
+  type:"interaction",
+  prototype:"slash",
+  code:`
+  Your modal test is below
+  $addButton[1;first visualization;primary;st;no]
+  $addButton[2;second visualization;2;primary;nd;no}
+  `
+  },{
+    type:"interaction",
+    prototype:"button",
+    code:`
+    $interactionModal[Modal test;modaltext;{actionRow:1
+{textInput:First visualization of an input:1:first:Oh... it's a litle div:yes::1(min characters):3(max characters)}
+},{actionRow:2
+{textInput:Second visualization of an input:2:second:Whaouh it's a big div:yes::1(min characters):200(max characters)}
+}]
+    $onlyIf[$checkContains[$interactionData[customId];visualization]==true;]
+    `
+  },{
+    name:"modaltext",
+    type:"interaction",
+    prototype:"modal",
+    code:`
+    	$interactionReply[<@$findUser[$textInputValue[first]]> => $textInputValue[second];;;;all]
+    `
+  }]
