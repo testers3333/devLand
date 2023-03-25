5bbc9712781a24ae2c8c7b6b3ba534451fc764d1
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-03-25 19:54:37 +0100

    Update modal.js

--- src/AOI.JS/Version 5.5.5/commands/basic/modal.js
+++ src/AOI.JS/Version 5.5.5/commands/basic/modal.js
@@ -1,26 +1,26 @@
-module.exports =[{
-  name:"modal",
-  code:`
+module.exports = [{
+  name: "modal",
+  code: `
   Your modal test is below
   $addButton[1;first visualization;primary;st_visualization;no]
   $addButton[2;second visualization;primary;nd_visualization;no]
   `
-  },{
-    type:"interaction",
-    prototype:"button",
-    code:`
+}, {
+  type: "interaction",
+  prototype: "button",
+  code: `
     $interactionModal[Modal test;modaltext;{actionRow:1
-{textInput:First visualization of an input:1:first:yes:Oh... it's a litle div::1(min characters):3(max characters)}
+{textInput:First visualization of an input:1:first:yes:Oh... it's a litle div:1:3}
 },{actionRow:2
-{textInput:Second visualization of an input:2:second:yes:Whaouh it's a big div::1(min characters):200(max characters)}
+{textInput:Second visualization of an input:2:second:yes:Whaouh it's a big div:1:200}
 }]
     $onlyIf[$checkContains[$interactionData[customId];visualization]==true;]
     `
-  },{
-    name:"modaltext",
-    type:"interaction",
-    prototype:"modal",
-    code:`
+}, {
+  name: "modaltext",
+  type: "interaction",
+  prototype: "modal",
+  code: `
     	$interactionReply[$textInputValue[first] => $textInputValue[second];;;;all]
     `
-  }]
+}]
