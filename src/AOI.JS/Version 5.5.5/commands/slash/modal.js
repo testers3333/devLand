f261e3c262f39408f566eb93d478d1e53e543069
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-03-25 12:17:03 +0100

    Update modal.js

--- src/AOI.JS/Version 5.5.5/commands/slash/modal.js
+++ src/AOI.JS/Version 5.5.5/commands/slash/modal.js
@@ -16,9 +16,9 @@ module.exports =[{
     prototype:"button",
     code:`
     $interactionModal[Modal test;modaltext;{actionRow:1
-{textInput:First visualization of an input:1:first:Oh... it's a litle div:yes::1(min characters):3(max characters)}
+{textInput:First visualization of an input:1:first:yes:Oh... it's a litle div::1(min characters):3(max characters)}
 },{actionRow:2
-{textInput:Second visualization of an input:2:second:Whaouh it's a big div:yes::1(min characters):200(max characters)}
+{textInput:Second visualization of an input:2:second:yes:Whaouh it's a big div::1(min characters):200(max characters)}
 }]
     $onlyIf[$checkContains[$interactionData[customId];visualization]==true;]
     `
